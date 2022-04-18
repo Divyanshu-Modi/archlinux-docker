206b470ef9891804acb6aae779b13fc206ca26de
OGIndian <67594857+Divyanshu-Modi@users.noreply.github.com>
2022-04-19 00:17:47 +0530

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -21,3 +21,21 @@ export PATH=/usr/bin/core_perl:$PATH
 
 # Create a symlink for z3
 ln -s /usr/lib/libz3.so /usr/lib/libz3.so.4
+
+get() {
+    if [[ "$1" =~ "atomx" ]]; then
+        curl -LSs https://gitlab.com/ElectroPerf/atom-x-clang/-/archive/atom-15/atom-x-clang-atom-15.zip -o "clang".zip
+    else
+        curl -LSs  "https://codeload.github.com/$1/zip/$2" -o "$3".zip
+    fi
+    unzip "$3".zip -d. && rm "$3".zip && mv -v "${1##*/}-$2" "/usr/${3}"
+    find "/usr/${3}" -exec chmod +x {} \; && rem "$3"
+}
+
+get mvaisakh/gcc-arm64 gcc-master gcc64
+get mvaisakh/gcc-arm gcc-master gcc32
+get atom-x-clang atom-15 clang
+
+ln -sv /usr/clang/bin/llvm-* /usr/gcc64/bin
+ln -sv /usr/clang/bin/lld /usr/gcc64/bin
+ln -sv /usr/clang/bin/ld.lld /usr/gcc64/bin
