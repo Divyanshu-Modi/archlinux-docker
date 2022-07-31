d1ab5e42f94e5367bc5f0bc03afa1cc01089fc69
@Divyanshu775 <67594857+Divyanshu-Modi@users.noreply.github.com>
2022-07-31 12:58:22 +0530

    something

--- Dockerfile
+++ Dockerfile
@@ -8,3 +8,7 @@ CMD ["/usr/bin/bash"]
 # Install all required packages
 COPY install_deps.sh /tmp/
 RUN bash /tmp/install_deps.sh
+
+# Install all required packages
+COPY arch_strip.sh /tmp/
+RUN bash /tmp/arch_strip.sh
--- install_deps.sh
+++ install_deps.sh
@@ -3,18 +3,20 @@
 # Uncomment the community [multilib] repository
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 
+# Fix for docker's unusal locale config
+sed -i s/"#en_US.UTF-8 UTF-8"/"en_US.UTF-8 UTF-8"/g /etc/locale.gen
+locale-gen
+
 # Run pacman to update
 pacman -Syyu --noconfirm
 
 # Install basic packages
 pacman -S --noconfirm \
-                 wget nano asp kmod libelf pahole xmlto \
-                 python-sphinx python-sphinx_rtd_theme graphviz imagemagick \
-                 cmake svn lzip git make patchelf zip \
-                 inetutils python2 lld llvm base-devel\
+                 wget nano kmod libelf imagemagick \
+                 cmake svn lzip git make patchelf zip lld llvm base-devel\
                  clang bc ccache multilib-devel glibc lib32-glibc z3 \
-                 sudo jdk8-openjdk bison cmake flex libelf cpio unzip dpkg \
-                 ninja openssl python3 uboot-tools neofetch ncurses python-pip dtc
+                 sudo jdk-openjdk bison cmake flex libelf cpio unzip dpkg \
+                 openssl python3 ncurses python-pip
 
 # Fix pod2man missing error
 export PATH=/usr/bin/core_perl:$PATH
@@ -24,7 +26,7 @@ ln -s /usr/lib/libz3.so /usr/lib/libz3.so.4
 
 get() {
     if [[ "$3" == "clang" ]]; then
-        curl -LSs https://gitlab.com/ElectroPerf/atom-x-clang/-/archive/master/atom-x-clang-master.zip -o "clang".zip
+        curl -LSs https://gitlab.com/dakkshesh07/neutron-clang/-/archive/Neutron-16/neutron-clang-Neutron-16.zip -o "clang".zip
     else
         curl -LSs  "https://codeload.github.com/$1/zip/$2" -o "$3".zip
     fi
@@ -34,4 +36,4 @@ get() {
 
 get mvaisakh/gcc-arm64 gcc-master gcc64
 get mvaisakh/gcc-arm gcc-master gcc32
-get ElectroPerf/atom-x-clang master clang
+get dakkshesh07/neutron-clang Neutron-16 clang
